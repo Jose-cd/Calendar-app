896862ee4d853a69f8e634e65b4245d02e586e14
jose-cd <josesaygg@gmail.com>
2021-06-02 21:14:16 -0500

    Mongoose connection

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,25 @@ import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import accessEnv from "./utils/accessEnv";
 import routes from "./routes";
+import mongoose from "mongoose";
 
 const PORT = parseInt(accessEnv("PORT", "7000"), 10);
+const MONGO_DB_USER = accessEnv("MONGO_DB_USER", "");
+const MONGO_DB_PW = accessEnv("MONGO_DB_PW", "");
+
+mongoose.connect(
+  `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PW}@cluster0.jupbx.mongodb.net/Cluster0?retryWrites=true&w=majority`,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw new Error("Ha ocurrido un error ");
+    console.log("connected to DB");
+    startServer();
+  }
+);
 
 const startServer = async () => {
   const app = express();
@@ -29,5 +46,3 @@ const startServer = async () => {
     console.info(`Server is listening on port ${PORT}`);
   });
 };
-
-startServer();
